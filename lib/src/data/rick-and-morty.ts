6836cf6a32b5ab42896a1ac34a03c27ef493c13f
Daniel Rigberg <daniel.rigberg@gmail.com>
2021-01-18 13:52:26 +0100

    lint and compile

--- lib/src/data/rick-and-morty.ts
+++ lib/src/data/rick-and-morty.ts
@@ -13,228 +13,228 @@ import { UniverseData } from '../interface'
 export const data: UniverseData = {
   places: [
     {
-      name: "The Citadel",
-      type: "city"
+      name: 'The Citadel',
+      type: 'city'
     },
     {
-      name: "Seattle",
-      type: "city"
+      name: 'Seattle',
+      type: 'city'
     },
     {
-      name: "Bespin",
-      type: "city"
+      name: 'Bespin',
+      type: 'city'
     },
     {
-      name: "Gazorpazorp",
-      type: "realm"
+      name: 'Gazorpazorp',
+      type: 'realm'
     },
     {
-      name: "Earth",
-      type: "realm"
+      name: 'Earth',
+      type: 'realm'
     },
     {
-      name: "Pluto",
-      type: "realm"
+      name: 'Pluto',
+      type: 'realm'
     },
     {
-      name: "Bespin",
-      type: "realm"
+      name: 'Bespin',
+      type: 'realm'
     },
     {
-      name: "Bird World",
-      type: "realm"
+      name: 'Bird World',
+      type: 'realm'
     },
     {
-      name: "Dwarf Terrace 9",
-      type: "realm"
+      name: 'Dwarf Terrace 9',
+      type: 'realm'
     },
     {
-      name: "Glaagablaaga",
-      type: "realm"
+      name: 'Glaagablaaga',
+      type: 'realm'
     },
     {
-      name: "Squanch",
-      type: "realm"
+      name: 'Squanch',
+      type: 'realm'
     }
   ],
   names: [
     {
-      first: "Rick",
-      last: "Sanchez"
+      first: 'Rick',
+      last: 'Sanchez'
     },
     {
-      first: "Jerry",
-      last: "Smith"
+      first: 'Jerry',
+      last: 'Smith'
     },
     {
-      first: "Mr. Meeseeks"
+      first: 'Mr. Meeseeks'
     },
     {
-      first: "Scary",
-      last: "Terry"
+      first: 'Scary',
+      last: 'Terry'
     },
     {
-      first: "Xenon",
-      last: "Bloom"
+      first: 'Xenon',
+      last: 'Bloom'
     },
     {
-      first: "Abradolf",
-      last: "Lincler"
+      first: 'Abradolf',
+      last: 'Lincler'
     },
     {
-      first: "Squanchy"
+      first: 'Squanchy'
     },
     {
-      first: "Unity"
+      first: 'Unity'
     },
     {
-      first: "Jessica"
+      first: 'Jessica'
     },
     {
-      first: "Ethan"
+      first: 'Ethan'
     },
     {
-      first: "Evil Morty"
+      first: 'Evil Morty'
     },
     {
-      first: "Birdperson"
+      first: 'Birdperson'
     },
     {
-      first: "Revolio",
-      last: "Clockberg"
+      first: 'Revolio',
+      last: 'Clockberg'
     },
     {
-      first: "Krombopulos",
-      last: "Michael"
+      first: 'Krombopulos',
+      last: 'Michael'
     },
     {
-      first: "Blim",
-      last: "Blam"
+      first: 'Blim',
+      last: 'Blam'
     },
     {
-      first: "Mr. Poopybutthole"
+      first: 'Mr. Poopybutthole'
     },
     {
-      first: "Crocubot"
+      first: 'Crocubot'
     },
     {
-      first: "Supernova"
+      first: 'Supernova'
     },
     {
-      first: "Million Ants"
+      first: 'Million Ants'
     },
     {
-      first: "Ants-In-My-Eyes",
-      last: "Johnson"
+      first: 'Ants-In-My-Eyes',
+      last: 'Johnson'
     },
     {
-      first: "Noob",
-      last: "Noob"
+      first: 'Noob',
+      last: 'Noob'
     },
     {
-      first: "Gazorpazorpfield"
+      first: 'Gazorpazorpfield'
     }
   ],
   items: [
     {
-      name: "plumbus",
-      type: "tool"
+      name: 'plumbus',
+      type: 'tool'
     },
     {
-      name: "cognition amplifier",
-      type: "tool"
+      name: 'cognition amplifier',
+      type: 'tool'
     },
     {
-      name: "dream inceptor",
-      type: "tool"
+      name: 'dream inceptor',
+      type: 'tool'
     },
     {
-      name: "shrink ray",
-      type: "tool"
+      name: 'shrink ray',
+      type: 'tool'
     },
     {
-      name: "Meeseeks box",
-      type: "tool"
+      name: 'Meeseeks box',
+      type: 'tool'
     },
     {
-      name: "ionic defibulizer",
-      type: "tool"
+      name: 'ionic defibulizer',
+      type: 'tool'
     },
     {
-      name: "microverse battery",
-      type: "tool"
+      name: 'microverse battery',
+      type: 'tool'
     },
     {
-      name: "combat suit",
-      type: "weapon"
+      name: 'combat suit',
+      type: 'weapon'
     },
     {
-      name: "portal gun",
-      type: "tool"
+      name: 'portal gun',
+      type: 'tool'
     },
     {
-      name: "neutrino bomb",
-      type: "weapon"
+      name: 'neutrino bomb',
+      type: 'weapon'
     },
     {
-      name: "laser gun",
-      type: "weapon"
+      name: 'laser gun',
+      type: 'weapon'
     },
     {
-      name: "freeze ray",
-      type: "weapon"
+      name: 'freeze ray',
+      type: 'weapon'
     },
     {
-      name: "pulse rifle",
-      type: "weapon"
+      name: 'pulse rifle',
+      type: 'weapon'
     },
     {
-      name: "space cruiser",
-      type: "vehicle"
+      name: 'space cruiser',
+      type: 'vehicle'
     }
   ],
  species: [
     {
-      name: "human",
-      type: "sentient"
+      name: 'human',
+      type: 'sentient'
     },
     {
-      name: "cromulon",
-      type: "sentient"
+      name: 'cromulon',
+      type: 'sentient'
     },
     {
-      name: "gazorpian",
-      type: "sentient"
+      name: 'gazorpian',
+      type: 'sentient'
     },
     {
-      name: "garblovian",
-      type: "sentient"
+      name: 'garblovian',
+      type: 'sentient'
     },
     {
-      name: "fleeb",
-      type: "nonsentient"
+      name: 'fleeb',
+      type: 'nonsentient'
     },
     {
-      name: "eye worm",
-      type: "nonsentient"
+      name: 'eye worm',
+      type: 'nonsentient'
     }
   ],
   quotes: [
     {
-      text: "I made the bomb, Morty.",
-      speaker: "Rick Sanchez"
+      text: 'I made the bomb, Morty.',
+      speaker: 'Rick Sanchez'
     },
     {
-      text: "I mean, why would a Pop-Tart want to live inside a toaster, Rick?",
-      speaker: "Morty Smith"
+      text: 'I mean, why would a Pop-Tart want to live inside a toaster, Rick?',
+      speaker: 'Morty Smith'
     },
     {
-      text: "To live is to risk it all, otherwise you’re just an inert chunk of randomly assembled molecules drifting wherever the universe blows you.",
-      speaker: "Rick Sanchez"
+      text: 'To live is to risk it all, otherwise you’re just an inert chunk of randomly assembled molecules drifting wherever the universe blows you.',
+      speaker: 'Rick Sanchez'
     },
     {
-      text: "You pass butter.",
-      speaker: "Rick Sanchez"
+      text: 'You pass butter.',
+      speaker: 'Rick Sanchez'
     }
   ]
 }
--- src/data/star-wars.ts
+++ src/data/star-wars.ts
@@ -11,364 +11,364 @@ import { UniverseData } from '../interface'
 export const data: UniverseData = {
   places: [
     {
-      name: "Cloud City",
-      type: "city"
+      name: 'Cloud City',
+      type: 'city'
     },
     {
-      name: "Theed",
-      type: "city"
+      name: 'Theed',
+      type: 'city'
     },
     {
-      name: "Mos Eisley",
-      type: "city"
+      name: 'Mos Eisley',
+      type: 'city'
     },
     {
-      name: "Tipoca City",
-      type: "city"
+      name: 'Tipoca City',
+      type: 'city'
     },
     {
-      name: "Galactic City",
-      type: "city"
+      name: 'Galactic City',
+      type: 'city'
     },
     {
-      name: "Anchorhead",
-      type: "city"
+      name: 'Anchorhead',
+      type: 'city'
     },
     {
-      name: "Ahto City",
-      type: "city"
+      name: 'Ahto City',
+      type: 'city'
     },
     {
-      name: "Tattooine",
-      type: "realm"
+      name: 'Tattooine',
+      type: 'realm'
     },
     {
-      name: "Dantooine",
-      type: "realm"
+      name: 'Dantooine',
+      type: 'realm'
     },
     {
-      name: "Korriban",
-      type: "realm"
+      name: 'Korriban',
+      type: 'realm'
     },
     {
-      name: "Kamino",
-      type: "realm"
+      name: 'Kamino',
+      type: 'realm'
     },
     {
-      name: "Coruscant",
-      type: "realm"
+      name: 'Coruscant',
+      type: 'realm'
     },
     {
-      name: "Bespin",
-      type: "realm"
+      name: 'Bespin',
+      type: 'realm'
     },
     {
-      name: "Hoth",
-      type: "realm"
+      name: 'Hoth',
+      type: 'realm'
     },
     {
-      name: "Naboo",
-      type: "realm"
+      name: 'Naboo',
+      type: 'realm'
     },
     {
-      name: "Alderaan",
-      type: "realm"
+      name: 'Alderaan',
+      type: 'realm'
     },
     {
-      name: "Dagobah",
-      type: "realm"
+      name: 'Dagobah',
+      type: 'realm'
     }
   ],
   names: [
     {
-      first: "Anakin",
-      last: "Skywalker"
+      first: 'Anakin',
+      last: 'Skywalker'
     },
     {
-      first: "Han",
-      last: "Solo"
+      first: 'Han',
+      last: 'Solo'
     },
     {
-      first: "Leia",
-      last: "Organa"
+      first: 'Leia',
+      last: 'Organa'
     },
     {
-      first: "Padme",
-      last: "Amidala"
+      first: 'Padme',
+      last: 'Amidala'
     },
     {
-      first: "Jar Jar",
-      last: "Binks"
+      first: 'Jar Jar',
+      last: 'Binks'
     },
     {
-      first: "Jabba",
-      last: "The Hutt"
+      first: 'Jabba',
+      last: 'The Hutt'
     },
     {
-      first: "C-3PO"
+      first: 'C-3PO'
     },
     {
-      first: "R2-D2"
+      first: 'R2-D2'
     },
     {
-      first: "Lando",
-      last: "Calrissian"
+      first: 'Lando',
+      last: 'Calrissian'
     },
     {
-      first: "Chewbacca"
+      first: 'Chewbacca'
     },
     {
-      first: "Boba",
-      last: "Fett"
+      first: 'Boba',
+      last: 'Fett'
     },
     {
-      first: "Jango",
-      last: "Fett"
+      first: 'Jango',
+      last: 'Fett'
     },
     {
-      first: "Greedo"
+      first: 'Greedo'
     },
     {
-      first: "Qui-Gon",
-      last: "Jinn"
+      first: 'Qui-Gon',
+      last: 'Jinn'
     },
     {
-      first: "K-2SO"
+      first: 'K-2SO'
     },
     {
-      first: "Obi-Wan",
-      last: "Kenobi"
+      first: 'Obi-Wan',
+      last: 'Kenobi'
     },
     {
-      first: "L3-37"
+      first: 'L3-37'
     },
     {
-      first: "Sheev",
-      last: "Palpatine"
+      first: 'Sheev',
+      last: 'Palpatine'
     },
     {
-      first: "Darth Vader"
+      first: 'Darth Vader'
     },
     {
-      first: "Darth Maul"
+      first: 'Darth Maul'
     },
     {
-      first: "Count Dooku"
+      first: 'Count Dooku'
     },
     {
-      first: "Luke",
-      last: "Skywalker"
+      first: 'Luke',
+      last: 'Skywalker'
     },
     {
-      first: "Mace",
-      last: "Windu"
+      first: 'Mace',
+      last: 'Windu'
     },
     {
-      first: "Yoda"
+      first: 'Yoda'
     },
     {
-      first: "Rey"
+      first: 'Rey'
     },
     {
-      first: "Finn"
+      first: 'Finn'
     },
     {
-      first: "Kylo",
-      last: "Ren"
+      first: 'Kylo',
+      last: 'Ren'
     },
     {
-      first: "Poe",
-      last: "Dameron"
+      first: 'Poe',
+      last: 'Dameron'
     }
   ],
   items: [
     {
-      name: "lightsaber",
-      type: "weapon"
+      name: 'lightsaber',
+      type: 'weapon'
     },
     {
-      name: "blaster",
-      type: "weapon"
+      name: 'blaster',
+      type: 'weapon'
     },
     {
-      name: "bowcaster",
-      type: "weapon"
+      name: 'bowcaster',
+      type: 'weapon'
     },
     {
-      name: "vibroblade",
-      type: "weapon"
+      name: 'vibroblade',
+      type: 'weapon'
     },
     {
-      name: "force lightning",
-      type: "weapon"
+      name: 'force lightning',
+      type: 'weapon'
     },
     {
-      name: "quarterstaff",
-      type: "weapon"
+      name: 'quarterstaff',
+      type: 'weapon'
     },
     {
-      name: "ion disrupter rifle",
-      type: "weapon"
+      name: 'ion disrupter rifle',
+      type: 'weapon'
     },
     {
-      name: "Gaffi stick",
-      type: "weapon"
+      name: 'Gaffi stick',
+      type: 'weapon'
     },
     {
-      name: "Death Star",
-      type: "weapon"
+      name: 'Death Star',
+      type: 'weapon'
     },
     {
-      name: "X-wing",
-      type: "vehicle"
+      name: 'X-wing',
+      type: 'vehicle'
     },
     {
-      name: "Y-wing",
-      type: "vehicle"
+      name: 'Y-wing',
+      type: 'vehicle'
     },
     {
-      name: "Ebon Hawk",
-      type: "vehicle"
+      name: 'Ebon Hawk',
+      type: 'vehicle'
     },
     {
-      name: "Millenium Falcon",
-      type: "vehicle"
+      name: 'Millenium Falcon',
+      type: 'vehicle'
     },
     {
-      name: "imperial star destroyer",
-      type: "vehicle"
+      name: 'imperial star destroyer',
+      type: 'vehicle'
     },
     {
-      name: "starfreighter",
-      type: "vehicle"
+      name: 'starfreighter',
+      type: 'vehicle'
     },
     {
-      name: "pod racer",
-      type: "vehicle"
+      name: 'pod racer',
+      type: 'vehicle'
     },
     {
-      name: "TIE fighter",
-      type: "vehicle"
+      name: 'TIE fighter',
+      type: 'vehicle'
     },
     {
-      name: "A-wing",
-      type: "vehicle"
+      name: 'A-wing',
+      type: 'vehicle'
     },
     {
-      name: "tractor beam",
-      type: "tool"
+      name: 'tractor beam',
+      type: 'tool'
     },
     {
-      name: "battle armor",
-      type: "tool"
+      name: 'battle armor',
+      type: 'tool'
     },
     {
-      name: "aural amplifier",
-      type: "tool"
+      name: 'aural amplifier',
+      type: 'tool'
     },
     {
-      name: "neural band",
-      type: "tool"
+      name: 'neural band',
+      type: 'tool'
     }
   ],
   species: [
     {
-      name: "bantha",
-      type: "nonsentient"
+      name: 'bantha',
+      type: 'nonsentient'
     },
     {
-      name: "taun taun",
-      type: "nonsentient"
+      name: 'taun taun',
+      type: 'nonsentient'
     },
     {
-      name: "rancor",
-      type: "nonsentient"
+      name: 'rancor',
+      type: 'nonsentient'
     },
     {
-      name: "acklay",
-      type: "nonsentient"
+      name: 'acklay',
+      type: 'nonsentient'
     },
     {
-      name: "gizka",
-      type: "nonsentient"
+      name: 'gizka',
+      type: 'nonsentient'
     },
     {
-      name: "krayt dragon",
-      type: "nonsentient"
+      name: 'krayt dragon',
+      type: 'nonsentient'
     },
     {
-      name: "sarlacc",
-      type: "nonsentient"
+      name: 'sarlacc',
+      type: 'nonsentient'
     },
     {
-      name: "shyrack",
-      type: "nonsentient"
+      name: 'shyrack',
+      type: 'nonsentient'
     },
     {
-      name: "ewok",
-      type: "sentient"
+      name: 'ewok',
+      type: 'sentient'
     },
     {
-      name: "jawa",
-      type: "sentient"
+      name: 'jawa',
+      type: 'sentient'
     },
     {
-      name: "hutt",
-      type: "sentient"
+      name: 'hutt',
+      type: 'sentient'
     },
     {
-      name: "human",
-      type: "sentient"
+      name: 'human',
+      type: 'sentient'
     },
     {
-      name: "ewok",
-      type: "sentient"
+      name: 'ewok',
+      type: 'sentient'
     },
     {
-      name: "mandalorian",
-      type: "sentient"
+      name: 'mandalorian',
+      type: 'sentient'
     },
     {
-      name: "twi'lek",
-      type: "sentient"
+      name: 'twi\'lek',
+      type: 'sentient'
     }
   ],
   quotes: [
     {
-      text: "There is no try: only do.",
-      speaker: "Yoda"
+      text: 'There is no try: only do.',
+      speaker: 'Yoda'
     },
     {
-      text: "I know.",
-      speaker: "Han Solo"
+      text: 'I know.',
+      speaker: 'Han Solo'
     },
     {
-      text: "Would somebody get this big walking carpet out of my way.",
-      speaker: "Leia Organa"
+      text: 'Would somebody get this big walking carpet out of my way.',
+      speaker: 'Leia Organa'
     },
     {
-      text: "It's a trap!",
-      speaker: "Admiral Ackbar"
+      text: 'It\'s a trap!',
+      speaker: 'Admiral Ackbar'
     },
     {
-      text: "Either shut him up or shut him down.",
-      speaker: "Han Solo"
+      text: 'Either shut him up or shut him down.',
+      speaker: 'Han Solo'
     },
     {
-      text: "ou truly belong here with us among the clouds.",
-      speaker: "Lando Calrissian"
+      text: 'ou truly belong here with us among the clouds.',
+      speaker: 'Lando Calrissian'
     },
     {
-      text: "I can fix this.",
-      speaker: "Poe Dameron"
+      text: 'I can fix this.',
+      speaker: 'Poe Dameron'
     },
     {
-      text: "Be careful not to choke on your aspirations, Director.",
-      speaker: "Darth Vader"
+      text: 'Be careful not to choke on your aspirations, Director.',
+      speaker: 'Darth Vader'
     },
     {
-      text: "Into the garbage chute, fly boy.",
-      speaker: "Leia Organa"
+      text: 'Into the garbage chute, fly boy.',
+      speaker: 'Leia Organa'
     }
   ]
 }
