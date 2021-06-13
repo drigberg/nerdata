5c319d732b460ac1133c7e834fc2365775835f6b
Daniel Rigberg <daniel.rigberg@gmail.com>
2021-06-13 17:23:56 +0200

    castlevania data

---
+++ src/data/castlevania.ts
@@ -0,0 +1,196 @@
+/*
+ * Module dependencies
+ */
+
+import { UniverseData } from '../interface';
+
+/*
+ * Module
+ */
+
+export const data: UniverseData = {
+  places: [
+    {
+      name: "Dracula's Castle",
+      type: 'location',
+    },
+    {
+      name: 'Belmont Hold',
+      type: 'location',
+    },
+    {
+      name: 'Targoviste',
+      type: 'location',
+    },
+    {
+      name: 'Braila',
+      type: 'location',
+    },
+    {
+      name: 'Lindenfeld',
+      type: 'location',
+    },
+    {
+      name: 'Gresit',
+      type: 'location',
+    },
+    {
+      name: 'Danesti',
+      type: 'location',
+    },
+    {
+      name: 'Styria',
+      type: 'realm',
+    },
+    {
+      name: 'Wallachia',
+      type: 'realm',
+    },
+    {
+      name: 'Hell',
+      type: 'realm',
+    },
+    {
+      name: 'Infinite Corridor',
+      type: 'location',
+    },
+  ],
+  names: [
+    {
+      first: 'Vlad',
+      last: 'Tepes',
+    },
+    {
+      first: 'Lisa',
+      last: 'Tepes',
+    },
+    {
+      first: 'Adrien',
+      last: 'Tepes',
+    },
+    {
+      first: 'Dracula',
+    },
+    {
+      first: 'Alucard',
+    },
+    {
+      first: 'Trevor',
+      last: 'Belmont',
+    },
+    {
+      first: 'Sypha',
+      last: 'Belnades',
+    },
+    {
+      first: 'Hector',
+    },
+    {
+      first: 'Cezar',
+    },
+    {
+      first: 'Isaac',
+    },
+    {
+      first: 'Godbrand',
+    },
+    {
+      first: 'Carmilla',
+    },
+    {
+      first: 'Striga',
+    },
+    {
+      first: 'Lenore',
+    },
+    {
+      first: 'Morana',
+    },
+    {
+      first: 'The Captain',
+    },
+    {
+      first: 'Varney',
+    },
+    {
+      first: 'Dragan',
+    },
+    {
+      first: 'Ratko',
+    },
+    {
+      first: 'Sumi',
+    },
+    {
+      first: 'Taka',
+    },
+    {
+      first: 'Saint Germain',
+    },
+    {
+      first: 'Miranda',
+    },
+    {
+      first: 'Sala',
+    },
+    {
+      first: 'Greta',
+    },
+    {
+      first: 'Cho',
+    },
+    {
+      first: 'Zamfir',
+    },
+  ],
+  items: [
+    {
+      name: 'Morning Star',
+      type: 'weapon'
+    },
+    {
+      name: 'combat cross',
+      type: 'weapon'
+    },
+    {
+      name: 'day armor',
+      type: 'tool'
+    },
+    {
+      name: 'forgemaster dagger',
+      type: 'tool'
+    },
+    {
+      name: 'forgemaster hammer',
+      type: 'tool'
+    },
+    {
+      name: 'transmission mirror',
+      type: 'tool'
+    },
+    {
+      name: 'distance mirror',
+      type: 'vehicle'
+    }
+  ],
+  species: [
+    {
+      name: 'vampire',
+      type: 'sentient'
+    },
+    {
+      name: 'night creature',
+      type: 'sentient'
+    },
+    {
+      name: 'stone-eye cyclops',
+      type: 'nonsentient'
+    }
+  ],
+  quotes: [
+    {
+      text: 'Oh my god. I am going insane.',
+      speaker: 'Alucard',
+    },
+  ],
+};
